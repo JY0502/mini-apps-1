3aba23b1a2bf70605129cf8474456e8259f385d9
Jae Yoo <pppjqqq@gmail.com>
2020-06-08 11:38:26 -0700

    added click event to add X

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,3 +2,11 @@
 // the app detects a win or tie and displays an appropriate message
 // a button resets the game for a new round of gameplay
 
+// console.log('Hello App')
+// console.log(document.getElementById('1'))
+
+const replaceSpot = (id) => {
+  console.log('hello click');
+  console.log(id);
+  document.getElementById(id).innerHTML = 'X';
+}
