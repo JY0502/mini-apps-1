933d786724095878e0d66576177c898f01c5c74e
Jae Yoo <pppjqqq@gmail.com>
2020-06-08 17:44:04 -0700

    refactored a little

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -9,12 +9,9 @@ let takeTurn = true;
 let whoseTurn = 'x';
 let gameOn = true;
 let takenSpots = [];
-let xRow = {'1': 0, '2': 0, '3': 0};
-let xCol = {'1': 0, '2': 0, '3': 0};
-let xDiag = {'for': 0, 'back': 0};
-let oRow = {'1': 0, '2': 0, '3': 0};
-let oCol = {'1': 0, '2': 0, '3': 0};
-let oDiag = {'for': 0, 'back': 0};
+const Row = {'x1': 0, 'x2': 0, 'x3': 0, 'o1': 0, 'o2': 0, 'o3': 0};
+const Col = {'x1': 0, 'x2': 0, 'x3': 0, 'o1': 0, 'o2': 0, 'o3': 0};
+const Diag = {'xfor': 0, 'xback': 0, 'ofor': 0, 'oback': 0};
 let xScore = 0;
 let oScore = 0;
 document.getElementById('xScore').innerHTML = `X : ${xScore}`;
@@ -30,12 +27,9 @@ const restart = () => {
   }
   gameOn = true;
   takenSpots = [];
-  xRow = {'1': 0, '2': 0, '3': 0};
-  xCol = {'1': 0, '2': 0, '3': 0};
-  xDiag = {'for': 0, 'back': 0};
-  oRow = {'1': 0, '2': 0, '3': 0};
-  oCol = {'1': 0, '2': 0, '3': 0};
-  oDiag = {'for': 0, 'back': 0};
+  for (let key in Row) {Row[key] = 0;};
+  for (let key in Col) {Col[key] = 0;};
+  for (let key in Diag) {Diag[key] = 0;};
   document.getElementById('whoseTurnNow').innerHTML = `New Game Start, ${whoseTurn.toUpperCase()}'s turn`;
   document.getElementById('gameTable').innerHTML = originalHTML;
   document.getElementById('whoWon').innerHTML = '';
@@ -47,44 +41,27 @@ const replaceSpot = (id) => {
   const whoseTurnNow = 'whoseTurnNow';
   const whoWon = 'whoWon';
   if (gameOn) {
-    if (whoseTurn === 'x' && document.getElementById(id).innerHTML === 'Not taken') {
+    if (document.getElementById(id).innerHTML === 'Not taken') {
       document.getElementById(id).innerHTML = whoseTurn.toUpperCase();
       document.getElementById(whoseTurnNow).innerHTML = `${whoseTurn.toUpperCase()}'s turn`;
-      whoseTurn = 'o';
-      xRow[id[0]]++;
-      xCol[id[1]]++;
+      Row[whoseTurn + id[0]]++;
+      Col[whoseTurn + id[1]]++;
       if(id[0] === id[1]) {
-        xDiag.for++;
+        Diag[whoseTurn + 'for']++;
       }
       if (Number(id[0]) + Number(id[1]) === 4) {
-        xDiag.back++;
+        Diag[whoseTurn + 'back']++;
       }
-      if (xRow[id[0]] === 3 || xCol[id[1]] === 3 || xDiag.for === 3 || xDiag.back === 3) {
+      if (Row[whoseTurn + id[0]] === 3 || Col[whoseTurn + id[1]] === 3 || Diag[whoseTurn + 'for'] === 3 || Diag[whoseTurn + 'back'] === 3) {
         gameOn = false;
         document.getElementById(whoseTurnNow).innerHTML = "Game Over";
-        document.getElementById(whoWon).innerHTML = "Congratulations X wins!";
-        xScore++;
-        document.getElementById('xScore').innerHTML = `X : ${xScore}`;
-      }
-    }
-    if (whoseTurn === 'o' && document.getElementById(id).innerHTML === 'Not taken') {
-      document.getElementById(id).innerHTML = whoseTurn.toUpperCase();
-      document.getElementById(whoseTurnNow).innerHTML = `${whoseTurn.toUpperCase()}'s turn`;
-      whoseTurn = 'x';
-      oRow[id[0]]++;
-      oCol[id[1]]++;
-      if(id[0] === id[1]) {
-        oDiag.for++;
-      }
-      if (Number(id[0]) + Number(id[1]) === 4) {
-        oDiag.back++;
-      }
-      if (oRow[id[0]] === 3 || oCol[id[1]] === 3 || oDiag.for === 3 || oDiag.back === 3) {
-        gameOn = false;
-        document.getElementById(whoseTurnNow).innerHTML = "Game Over";
-        document.getElementById(whoWon).innerHTML = "Congratulations O wins!";
-        oScore++;
-        document.getElementById('oScore').innerHTML = `O : ${oScore}`;
+        document.getElementById(whoWon).innerHTML = `Congratulations ${whoseTurn.toUpperCase()} wins!`;
+        if (whoseTurn === 'x') {
+          xScore++;
+        } else {
+          oScore++;
+        }
+        document.getElementById(whoseTurn + 'Score').innerHTML = `${whoseTurn.toUpperCase()} : ${xScore}`;
       }
     }
     takenSpots.push(id);
@@ -94,5 +71,9 @@ const replaceSpot = (id) => {
       document.getElementById(whoseTurnNow).innerHTML = "Game Over";
     }
   }
+  if (whoseTurn === 'x') {
+    whoseTurn = 'o';
+  } else {
+    whoseTurn = 'x';
+  }
 }
-
